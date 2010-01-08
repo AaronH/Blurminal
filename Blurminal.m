8e89697fb37c7db6d78639d3844e127ec9565a09
Aaron Hurley <aaron@aaronhurley.com>
2010-01-08 14:54:25 -0800

    Add a disable blur function. Attempting to disable around expose with no luck

--- Blurminal.m
+++ Blurminal.m
@@ -4,21 +4,33 @@
 typedef void* CGSConnectionID;
 extern OSStatus CGSNewConnection (const void** attr, CGSConnectionID* id);
 
+uint32_t _myFilter;
+CGSConnectionID _myConnection;
+
 @implementation NSWindow (TTWindow)
 // Found here:
 // http://www.aeroxp.org/board/index.php?s=d18e98cabed9ce5ad27f9449b4e2298f&showtopic=8984&pid=116022&st=0&#entry116022
 - (void)enableBlur
-{
-	CGSConnectionID _myConnection;
-	CGSNewConnection(NULL , &_myConnection);
+{	
+	if (!_myConnection) CGSNewConnection(NULL , &_myConnection);
 
-	uint32_t __compositingFilter = 0;
-	CGSNewCIFilterByName (_myConnection, (CFStringRef)@"CIGaussianBlur", &__compositingFilter);
-
-	NSDictionary* optionsDict = [NSDictionary dictionaryWithObject:[[NSUserDefaults standardUserDefaults] objectForKey:@"Blurminal Radius"] forKey:@"inputRadius"];
-	CGSSetCIFilterValuesFromDictionary(_myConnection, __compositingFilter, (CFDictionaryRef)optionsDict);
+	if (!_myFilter){
+		CGSNewCIFilterByName (_myConnection, (CFStringRef)@"CIGaussianBlur", &_myFilter);
+		
+		NSDictionary* optionsDict = [NSDictionary dictionaryWithObject:[[NSUserDefaults standardUserDefaults] objectForKey:@"Blurminal Radius"] forKey:@"inputRadius"];
+		CGSSetCIFilterValuesFromDictionary(_myConnection, _myFilter, (CFDictionaryRef)optionsDict);		
+	}
 	
-	CGSAddWindowFilter(_myConnection, [self windowNumber], __compositingFilter, 1);
+	CGSAddWindowFilter(_myConnection, [self windowNumber], _myFilter, 1);
+	
+}
+
+- (void)disableBlur
+{
+	if (_myFilter && _myConnection) {
+		CGSRemoveWindowFilter(_myConnection, [self windowNumber], _myFilter);
+		CGSReleaseCIFilter(_myConnection, _myFilter);
+	}
 }
 
 - (void)enableBlurIfCorrectWindow:(BOOL)delay
@@ -44,7 +56,18 @@ extern OSStatus CGSNewConnection (const void** attr, CGSConnectionID* id);
 	return self;
 }
 
+- (void)Blurred_miniaturize:(id)sender;
+{
+	[self Blurred_miniaturize:sender];
+	[self disableBlur];
+}
+- (void)Blurred_deminiaturize:(id)sender;
+{
+	[self Blurred_deminiaturize:sender];
+	[self enableBlur];
+}
 @end
+
 
 @implementation Blurminal
 + (void)load
@@ -57,7 +80,8 @@ extern OSStatus CGSNewConnection (const void** attr, CGSConnectionID* id);
 		[NSNumber numberWithFloat:1.0],@"Blurminal Radius",
 		nil]];
 	[[NSWindow class] jr_swizzleMethod:@selector(initWithContentRect:styleMask:backing:defer:) withMethod:@selector(Blurred_initWithContentRect:styleMask:backing:defer:) error:NULL];
-	
-	
+//	[[NSWindow class] jr_swizzleMethod:@selector(miniaturize:) withMethod:@selector(Blurred_miniaturize:) error:NULL];
+//	[[NSWindow class] jr_swizzleMethod:@selector(deminiaturize:) withMethod:@selector(Blurred_deminiaturize:) error:NULL];
+							
 }
 @end
